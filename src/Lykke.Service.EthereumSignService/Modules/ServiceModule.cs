7e0f8359a492a05fefefbd85c04ff11ebd826ed0
Dmitry Smolyakov <dmitry.smolyakov@lykke.com>
2018-08-02 18:30:05 +0300

    LWDEV-8745 Disable all logging + reduce settings + update deps + migrate to core 2.1.

--- src/Lykke.Service.EthereumSignService/Modules/ServiceModule.cs
+++ src/Lykke.Service.EthereumSignService/Modules/ServiceModule.cs
@@ -1,29 +1,11 @@
 ﻿using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using Common.Log;
 using Lykke.Service.EthereumSignService.Core.Services;
-using Lykke.Service.EthereumSignService.Core.Settings.ServiceSettings;
 using Lykke.Service.EthereumSignService.Services;
-using Lykke.SettingsReader;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Lykke.Service.EthereumSignService.Modules
 {
     public class ServiceModule : Module
     {
-        private readonly IReloadingManager<EthereumSignServiceSettings> _settings;
-        private readonly ILog _log;
-        // NOTE: you can remove it if you don't need to use IServiceCollection extensions to register service specific dependencies
-        private readonly IServiceCollection _services;
-
-        public ServiceModule(IReloadingManager<EthereumSignServiceSettings> settings, ILog log)
-        {
-            _settings = settings;
-            _log = log;
-
-            _services = new ServiceCollection();
-        }
-
         protected override void Load(ContainerBuilder builder)
         {
             // TODO: Do not register entire settings in container, pass necessary settings to services which requires them
@@ -31,11 +13,7 @@ namespace Lykke.Service.EthereumSignService.Modules
             //  builder.RegisterType<QuotesPublisher>()
             //      .As<IQuotesPublisher>()
             //      .WithParameter(TypedParameter.From(_settings.CurrentValue.QuotesPublication))
-
-            builder.RegisterInstance(_log)
-                .As<ILog>()
-                .SingleInstance();
-
+            
             builder.RegisterType<HealthService>()
                 .As<IHealthService>()
                 .SingleInstance();
@@ -55,8 +33,6 @@ namespace Lykke.Service.EthereumSignService.Modules
 
             builder.RegisterType<WalletCreationService>()
                 .As<IWalletCreationService>().SingleInstance();
-
-            builder.Populate(_services);
         }
     }
 }
